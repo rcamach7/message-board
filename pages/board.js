4bb0af8940350f4eb7d45ed2c3ed7d75e1e33006
Ricardo Camacho Mireles <ricmireles7@gmail.com>
2022-08-12 12:43:38 -0700

    add basic layout to board page

--- pages/board.js
+++ pages/board.js
@@ -3,22 +3,36 @@ import axios from "axios";
 import Router from "next/router";
 
 export default function Board() {
-  const user = useUser({ redirectTo: "/" });
-
   const logout = async () => {
     await axios.get("/api/logout");
     Router.push("/");
   };
 
   return (
-    <div className="flex flex-col h-screen items-center justify-center">
-      <h1>You are logged in! Hello {user && user.username}</h1>
-      <button
-        className="text-sm font-extrabold mt-2 p-1 text-blue-400"
-        onClick={logout}
-      >
-        Logout
-      </button>
+    <div className="Board h-screen w-screen flex flex-col">
+      <nav className="navBar flex items-center text-center bg-slate-400 font-bold">
+        <h1 className="flex-1">Message Board</h1>
+        <button
+          onClick={logout}
+          className="logoutBtn p-2 border-l-2 border-cyan-900"
+        >
+          Logout
+        </button>
+      </nav>
+
+      <div className="messageContainer flex flex-col flex-1">
+        <p>This is the message container</p>
+      </div>
+
+      <form className="newMessage flex p-2">
+        <input
+          type="text"
+          className="messageInput grow pl-1 text-center border-2 border-gray-400 rounded-lg py-2 px-4 text-lg"
+        />
+        <button className="sendBtn flex-1 bg-blue-500 hover:bg-blue-700 text-white font-bold rounded m-1">
+          Send
+        </button>
+      </form>
     </div>
   );
 }
