efd82146c2116095223dcf20bc3333bc276a03bf
Ricardo Camacho Mireles <ricmireles7@gmail.com>
2022-08-15 14:40:19 -0700

    create basic message component only applying for current user

--- components/MessageCard.jsx
+++ components/MessageCard.jsx
@@ -1,13 +1,22 @@
 import React from "react";
 import Image from "next/image";
-import { useEffect } from "react";
+import Date from "./Date";
 
 export const MessageCard = ({ message, currentUser }) => {
   const { _id, text, timeStamp } = message;
   const { name, image, email } = message.user;
 
   return (
-    <div className="messageCard flex">
+    <div className="messageCard flex justify-end gap-1">
+      <div className="messageCard__text flex flex-col">
+        <p className="messageCard__text__name font-semibold text-xs text-gray-600">
+          {name}
+        </p>
+        <div className="messageCard__text__message bg-blue-400 p-1 rounded-lg">
+          <p className="messageCard__text__message__text text-white">{text}</p>
+        </div>
+        <Date dateString={timeStamp} />
+      </div>
       <Image
         className="pr-2 rounded-full"
         src={image}
@@ -15,10 +24,6 @@ export const MessageCard = ({ message, currentUser }) => {
         width={50}
         height={50}
       />
-      <div className="messageCard__text">
-        <div className="messageCard__text__name">{name}</div>
-        <div className="messageCard__text__message">{text}</div>
-      </div>
     </div>
   );
 };
